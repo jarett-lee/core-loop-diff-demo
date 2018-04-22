2c9ceee8685c933cef6a365e6a4fee50f86e55da
jarett-lee <jarett.lee.pi@gmail.com>
2018-04-22 01:01:05 -0400

    Add file structure and build system

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+// main.js
+
+import multiply from './multiply.js'
+
+console.log('2 * 3 = ' + multiply(2, 3)) // 6
---
+++ src/js/multiply.js
@@ -0,0 +1,5 @@
+// addition.js
+
+export default function multiply (a, b) {
+  return a * b
+}
